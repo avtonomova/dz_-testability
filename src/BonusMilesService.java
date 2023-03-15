d4a3efeddfd95ec931f5b65f822dbf91fe518181
Avtonomova <niva1090@mail.ru>
2023-03-15 14:00:32 +1000

    second commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,7 @@
 public class BonusMilesService {
 
     public int calculate(int cost) {
-        int price = 505000;
-        int miles = price / 20 * 1;
+        int miles = cost / 20;
         return miles;
 
     }
